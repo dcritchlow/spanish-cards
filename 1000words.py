5d5c94f87cdcf446a35cce04b0c17b39f9fd03db
Darin Critchlow <darincritchlow@gmail.com>
2015-07-10 12:18:33 -0600

    initial commit

---
+++ 1000words.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#title           :1000words.py
+#description     :download word and audio for top 1000 spanish words
+#author          :Darin Critchlow
+#date            :7/10/2015
+#=======================================================================
+# Import the modules needed to run the script.
+from lxml import html
+import requests
